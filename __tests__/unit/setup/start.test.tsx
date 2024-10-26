afa082f42fd4bda9f1dec752529f48db06462c25
partha04 <partha16896@gmail.com>
2024-10-26 22:53:52 +0530

    test: fix import expect from jest global

--- __tests__/unit/setup/start.test.tsx
+++ __tests__/unit/setup/start.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
-
+import { expect } from "@jest/globals";
 describe("Test jest setup", () => {
   test("should pass", () => {
     expect(true).toBeTruthy();
